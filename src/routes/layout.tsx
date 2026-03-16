935987d172ff01ba5658d5d0433163af0f531f63
sIUzy <justinpeligro.c04@gmail.com>
2026-03-16 22:23:50 +0800

    (fix): deployment error

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../components/navbar/navbar";
-import Footer from "../components/footer/footer";
+// import Footer from "../components/footer/footer";
 
 export default function Layout() {
   return (
